d83648623aea66da1c818a6259b6c63ab1b8df09
Sid <chinnusreddy@gmail.com>
2024-05-11 19:57:45 -0400

    requirements added

---
+++ app.py
@@ -0,0 +1,27 @@
+
+import os
+from typing import List
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.chains import (
+    ConversationalRetrievalChain,
+)
+from langchain.chat_models import ChatOpenAI
+
+from langchain.docstore.document import Document
+from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+
+import chainlit as cl
+
+print("all_ok")
+
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
